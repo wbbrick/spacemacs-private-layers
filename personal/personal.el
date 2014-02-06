9c72510fc9f0ecc9f438bceab3b690522b9f96f7
unknown <wbrick@po202.towson.edu>
2014-02-06 17:29:52 -0500

    Some extra functionality with doremi etc

--- personal/personal.el
+++ personal/personal.el
@@ -1,30 +1,38 @@
-
 ;;personalized in
 ;disable backup
 (setq backup-inhibited t)
 ;disable auto save
 (setq auto-save-default nil)
 
-(remove-hook 'find-file-hooks 'vc-find-file-hook)
-(custom-set-variables
-;; custom-set-variables was added by Custom.
-;; If you edit it by hand, you could mess it up, so be careful.
-;; Your init file should contain only one such instance.
-;; If there is more than one, they won't work right.
-'(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
-'(custom-enabled-themes (quote (wombat))))
-(custom-set-faces
-;; custom-set-faces was added by Custom.
-;; If you edit it by hand, you could mess it up, so be careful.
-;; Your init file should contain only one such instance.
-;; If there is more than one, they won't work right.
-)
+ (remove-hook 'find-file-hooks 'vc-find-file-hook)
+;; (custom-set-variables
+;; ;; custom-set-variables was added by Custom.
+;; ;; If you edit it by hand, you could mess it up, so be careful.
+;; ;; Your init file should contain only one such instance.
+;; ;; If there is more than one, they won't work right.
+;; '(ansi-color-names-vector ["#212526" "#ff4b4b" "#b4fa70" "#fce94f" "#729fcf" "#ad7fa8" "#8cc4ff" "#eeeeec"])
+;; '(custom-enabled-themes (quote (wombat))))
+;; (custom-set-faces
+;; ;; custom-set-faces was added by Custom.
+;; ;; If you edit it by hand, you could mess it up, so be careful.
+;; ;; Your init file should contain only one such instance.
+;; ;; If there is more than one, they won't work right.
+;; )
 
 (require 'package)
 (add-to-list 'package-archives
              '("marmalade" .
                "http://marmalade-repo.org/packages/"))
 (package-initialize)
+
+;(require 'js2-mode)
+;(require 'html-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.ejs\\'" . html-mode))
+
+(defun turn-on-ac-js2 () (ac-js2-mode 1))
+(add-hook 'js2-mode-hook 'turn-on-ac-js2)
+(add-hook 'windmove-hook 'crosshairs-flash)
 
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
@@ -36,14 +44,11 @@
 (define-key my-keys-minor-mode-map (kbd "M-i") 'previous-line) ; was tab-to-tab-stop
 (define-key my-keys-minor-mode-map (kbd "M-k") 'next-line) ; was kill-sentence
 
-
 (define-minor-mode my-keys-minor-mode
   "A minor mode so that my key settings override annoying major modes."
   t " my-keys" 'my-keys-minor-mode-map)
 
 (defun open-external-explorer () (interactive) (shell-command "explorer ."))
-
-
 
 (my-keys-minor-mode 1)
 
@@ -57,6 +62,8 @@
 
 (global-set-key (kbd "M-SPC") 'set-mark-command) ; was just-one-space
 (global-set-key (kbd "<f8>") 'execute-extended-command)
+(global-set-key (kbd "M-`") 'doremi-buffers+)
+(global-set-key (kbd "C-`") 'doremi-window-height+)
 
 (global-set-key (kbd "<C-tab>") 'next-multiframe-window)
 (global-set-key (kbd "<C-S-tab>") 'previous-multiframe-window)
@@ -84,9 +91,6 @@
 (other-window 1)
 
 ;;NOTES
-
-;;to get to the dropbox file modify your emacs shortcut to look something like this:
-;;C:\devtools\emacs-24.3\bin\runemacs.exe -q -l "C:\Users\wbrick.wbrick-laptop\Dropbox\.emacs"
 
 ;;wombat is a good dark medium-contrast theme, comes built in
 ;;recommended packages:
