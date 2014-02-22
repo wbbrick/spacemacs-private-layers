238613e79c114659257484243924cc7529ac16ae
Bryan Brick <wbbrick@towson.edu>
2014-02-21 23:19:31 -0500

    More extra functionality

--- personal/personal.el
+++ personal/personal.el
@@ -30,9 +30,7 @@
 
 (defun turn-on-ac-js2 () (auto-complete-mode 1) (ac-js2-mode 1))
 
-(add-hook 'js2-mode-hook 'turn-on-ac-js2)
-(add-hook 'windmove-hook 'crosshairs-flash)
-
+(add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)
 
 (if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
@@ -51,6 +49,11 @@
 (defun open-external-explorer () (interactive) (shell-command "explorer ."))
 (defun open-personal-file () (interactive) (find-file "~/.emacs.d/personal/personal.el"))
 
+(defun clean-js-file () (interactive)
+  (delete-trailing-whitespace)
+  (query-replace "\"" "'"  nil (point-min) (point-max))
+  )
+
 (my-keys-minor-mode 1)
 
 (global-set-key (kbd "M-j") 'backward-char) ; was indent-new-comment-line
@@ -65,12 +68,14 @@
 (global-set-key (kbd "<f8>") 'execute-extended-command)
 (global-set-key (kbd "M-`") 'doremi-buffers+)
 (global-set-key (kbd "C-`") 'doremi-window-height+)
+(global-set-key (kbd "C-q") 'kill-this-buffer)
 
 (global-set-key (kbd "<C-tab>") 'next-multiframe-window)
 (global-set-key (kbd "<C-S-tab>") 'previous-multiframe-window)
 
 (global-set-key [f12] 'open-external-explorer)
 (global-set-key [f11] 'open-personal-file)
+(global-set-key [f10] 'delete-trailing-whitespace)
 (global-set-key [f5] 'call-last-kbd-macro)
 
 
