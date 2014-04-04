b6c7b9b431ad3e0c06de6094da3b3c9796172f81
Bryan Brick <wbbrick@towson.edu>
2014-04-04 14:34:44 -0400

    Finally fixed my dired problem

--- custom.el
+++ custom.el
@@ -6,7 +6,6 @@
  '(custom-enabled-themes (quote (wombat)))
  '(dired-listing-switches "-l")
  '(eshell-load-hook nil)
- '(eshell-ls-use-in-dired t nil (em-ls))
  '(fci-rule-color "#383838")
  '(vc-annotate-background "#2B2B2B")
  '(vc-annotate-color-map (quote ((20 . "#BC8383") (40 . "#CC9393") (60 . "#DFAF8F") (80 . "#D0BF8F") (100 . "#E0CF9F") (120 . "#F0DFAF") (140 . "#5F7F5F") (160 . "#7F9F7F") (180 . "#8FB28F") (200 . "#9FC59F") (220 . "#AFD8AF") (240 . "#BFEBBF") (260 . "#93E0E3") (280 . "#6CA0A3") (300 . "#7CB8BB") (320 . "#8CD0D3") (340 . "#94BFF3") (360 . "#DC8CC3"))))
