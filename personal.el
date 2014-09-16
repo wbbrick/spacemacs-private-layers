5cc4ed4ad167d480b28e167b65e199fc0a71e9a2
Bryan Brick <wbbrick@gmail.com>
2014-09-16 15:42:07 -0400

    helm config

--- personal.el
+++ personal.el
@@ -43,6 +43,16 @@
 
 (global-set-key (kbd "C-x g") 'webjump)
 
+(setq helm-quick-update                     t ; do not display invisible candidates
+      helm-split-window-in-side-p           t ; open helm buffer inside current window, not occupy whole other window
+      helm-buffers-fuzzy-matching           t ; fuzzy matching buffer names when non--nil
+      helm-move-to-line-cycle-in-source     t ; move to end or beginning of source when reaching top or bottom of source.
+      helm-ff-search-library-in-sexp        t ; search for library in `require' and `declare-function' sexp.
+      helm-scroll-amount                    4 ; scroll 8 lines other window using M-<next>/M-<prior>
+      helm-ff-file-name-history-use-recentf t)
+
+(helm-mode 1)
+
 ; Add DuckDuckGo to webjump
 (eval-after-load "webjump"
   '(add-to-list 'webjump-sites
