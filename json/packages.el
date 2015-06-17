00da892c0701b8f5b0c5f5c8adb774387d58ddb0
Bryan Brick <wbbrick@gmail.com>
2015-06-17 14:44:26 -0400

    Got the json mode indenting right

--- json/packages.el
+++ json/packages.el
@@ -24,7 +24,24 @@
 ;;
 (defun personal-config/init-personal-config ()
   "Initialize my package"
+  (add-to-list 'auto-mode-alist '("\\.json\\'" . json-mode))
+  
+  (add-hook 'json-mode-hook
+            '(lambda ()
+               ;; Setting indentation lvel
+               (setq c-basic-offset 2)
 
+               (setq tab-width 2)
+
+               (make-local-variable 'js-indent-level)
+               (setq js-indent-level 2)
+
+
+               ;; Make TAB equivilent to 4 spaces
+               (setq indent-tabs-mode nil)
+
+               )
+            )
   )
 ;;
 ;; Often the body of an initialize function uses `use-package'
